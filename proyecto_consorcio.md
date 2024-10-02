b66448f2324bb3fd8cb7c38dd904f8e75014f008
Gaston Morales <104536608+TongaCasla@users.noreply.github.com>
2024-10-02 16:30:36 -0300

    Primera entrega del proyecto
    Contiene:
    -script de modelo de datos
    -script de insert de datos
    -diccionario de datos
    -diagrama de entidad-relacion
    -informe de capitulo 1 en formato .md

---
+++ proyecto_consorcio.md
@@ -0,0 +1,44 @@
+# Proyecto de Estudio
+    
+
+# PRESENTACIÓN (Proyecto Consorcio)
+
+**Asignatura**: Bases de Datos I (FaCENA-UNNE)
+
+**Integrantes**:
+ -Dusicka, Marisol 
+ -Miltos, Sonia
+ -Morales, Alan Gaston
+ -Mordacini Gianella
+
+**Año**: 2024
+
+## CAPÍTULO I: INTRODUCCIÓN
+
+### Caso de estudio
+
+El objetivo principal es explorar en profundidad el uso de vistas y vistas indexadas,
+así como su optimización en este sistema de gestión de bases de datos. Estas
+herramientas desempeñan un papel esencial en la administración eficiente de datos
+y la mejora del rendimiento de las consultas, lo que las convierte en un área de
+interés fundamental para los profesionales y estudiantes de bases de datos.
+
+### Definición o planteamiento del problema
+
+Las vistas de SQL Server son una forma poderosa de organizar datos y crear
+consultas personalizadas. Con las vistas, puede unir varias tablas, establecer
+condiciones e incluso controlar el acceso del usuario a los datos.
+Comúnmente para simplificar la complejidad de una consulta SQL y para hacer que
+los datos sean más fáciles de leer y entender. También son útiles para la seguridad,
+ya que se pueden utilizar para restringir el acceso a ciertas partes de la base de
+datos.
+
+### Objetivos
+El objetivo principal de este Trabajo Práctico es investigar y analizar a fondo el uso,
+la optimización y la aplicación de vistas y vistas indexadas en bases de datos, con
+un enfoque específico en SQL Server 2019. Se busca comprender cómo estas
+herramientas pueden mejorar el rendimiento y la gestión de bases de datos
+relacionales.
+
+
+
---
+++ script/script_ddl_proyecto.sql
@@ -0,0 +1,126 @@
+CREATE DATABASE bd_proyecto_consorcio;
+go
+USE bd_proyecto_consorcio;
+go
+-------------------
+if object_id('gasto') is not null DROP TABLE gasto;
+go
+---------------------
+if object_id('consorcio') is not null DROP TABLE consorcio
+go
+---------------------
+if object_id('localidad') is not null DROP TABLE localidad
+go
+---------------------
+if object_id('provincia') is not null DROP TABLE provincia
+go
+---------------------
+if object_id('zona') is not null DROP TABLE zona
+go
+---------------------
+if object_id('conserje') is not null DROP TABLE conserje
+go
+---------------------
+if object_id('administrador') is not null DROP TABLE administrador
+go
+---------------------
+if object_id('tipogasto') is not null DROP TABLE tipogasto
+go
+---------------------
+
+Create table provincia (idprovincia int primary key, 
+					    descripcion varchar(50),
+						km2 int,
+						cantdptos int,
+						poblacion int,
+						nomcabe varchar(50))
+go
+-------------------
+
+Create table localidad (idprovincia int, 
+						idlocalidad int, 
+					    descripcion varchar(50),
+					    Constraint PK_localidad PRIMARY KEY (idprovincia, idlocalidad),
+						Constraint FK_localidad_pcia FOREIGN KEY (idprovincia)  REFERENCES provincia(idprovincia)						 					     					     					     				     					     
+						)
+go
+-------------------
+
+Create table zona		(idzona int identity primary key, 
+					    descripcion varchar(50))
+go
+-------------------
+
+Create table conserje	(idconserje int identity primary key, 
+					     apeynom varchar(50),
+					     tel varchar(20),
+						 fechnac datetime,
+					     estciv varchar(1)  NULL default ('S') 
+						 CONSTRAINT CK_estadocivil CHECK (estciv IN ('S', 'C','D','O')),
+							 	)
+go
+-------------------
+
+Create table administrador	(idadmin int identity primary key, 
+					     apeynom varchar(50),
+					     viveahi varchar(1)  NULL default ('N') 
+						 CONSTRAINT CK_habitante_viveahi CHECK (viveahi IN ('S', 'N')),
+					     tel varchar(20),
+					     sexo varchar(1)  NOT NULL 
+						 CONSTRAINT CK_sexo CHECK (sexo IN ('F', 'M')),
+                         fechnac datetime)
+
+go
+-------------------
+
+Create table tipogasto	(idtipogasto int primary key, 
+					    descripcion varchar(50))
+go
+-------------------
+
+Create table consorcio	(idprovincia int,
+                         idlocalidad int,
+                         idconsorcio int, 
+					     nombre varchar(50),
+					     direccion varchar(250),					     
+					     idzona int,	
+						 idconserje int,	
+						 idadmin int,	
+					     Constraint PK_consorcio PRIMARY KEY (idprovincia, idlocalidad,idconsorcio),
+						 Constraint FK_consorcio_pcia FOREIGN KEY (idprovincia,idlocalidad)  REFERENCES localidad(idprovincia,idlocalidad),
+						 Constraint FK_consorcio_zona FOREIGN KEY (idzona)  REFERENCES zona(idzona),						 					     					     					     				     					     
+						 Constraint FK_consorcio_conserje FOREIGN KEY (idconserje)  REFERENCES conserje(idconserje),
+						 Constraint FK_consorcio_admin FOREIGN KEY (idadmin)  REFERENCES administrador(idadmin)						 					     					     					     				     					     						 						 						 					     					     					     				     					     						 
+							)
+go
+-------------------
+
+Create table gasto	(
+						idgasto int identity,
+						idprovincia int,
+                         idlocalidad int,
+                         idconsorcio int, 
+					     periodo int,
+					     fechapago datetime unique,					     
+						 idtipogasto int,
+						 importe decimal (8,2),	
+					     Constraint PK_gasto PRIMARY KEY (idgasto),
+						 Constraint FK_gasto_consorcio FOREIGN KEY (idprovincia,idlocalidad,idconsorcio)  REFERENCES consorcio(idprovincia,idlocalidad,idconsorcio),
+						 Constraint FK_gasto_tipo FOREIGN KEY (idtipogasto)  REFERENCES tipogasto(idtipogasto)					     					     						 					     					     
+							)
+go
+
+CREATE TABLE inmueble(
+	 idprovincia INT NOt NULL,
+	 idlocalidad INT NOT NULL,
+	 idconsorcio INT NOT NULL,
+	 sup DECIMAL(6,2),
+	 nro_pisos INT,
+	 cant_dpto INT,
+	 espacio_comun INT NOT NULL
+ );
+
+ALTER TABLE inmueble
+	ADD CONSTRAINT PK_inmueble PRIMARY KEY (idprovincia, idlocalidad, idconsorcio);
+ALTER TABLE inmueble
+	ADD CONSTRAINT FK_inmueble_consorcio FOREIGN KEY(idprovincia, idlocalidad, idconsorcio)REFERENCES consorcio(idprovincia, idlocalidad, idconsorcio);

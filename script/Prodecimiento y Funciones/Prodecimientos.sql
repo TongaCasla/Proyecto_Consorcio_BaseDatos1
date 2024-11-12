b218b8e9fb9fe14fc5d07bba3862cdba69d93c32
Gaston Morales <104536608+TongaCasla@users.noreply.github.com>
2024-11-11 22:53:31 -0300

    Procedimiento y Funciones
    3 Procedimientos (insert,update,delete) sobre una tabla
    3 Funciones de tipo tabla
    1 Procedimiento de calculo de edad
    1 Funcion de calculo de edad
    Sentencias de prueba

--- script/Prodecimiento y Funciones/Prodecimientos.sql
+++ script/Prodecimiento y Funciones/Prodecimientos.sql
@@ -20,9 +20,13 @@ CREATE PROCEDURE  InsertGasto
 	@importe decimal (8,2)
 AS
 BEGIN
-	insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe)
-	values(@id_provincia,@id_localidad,@id_consorcio,@periodo,@fecha_pago,@id_tipogasto,@importe)
-	
+	BEGIN TRY
+		insert into gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe)
+		values(@id_provincia,@id_localidad,@id_consorcio,@periodo,@fecha_pago,@id_tipogasto,@importe)
+	END TRY
+	BEGIN CATCH
+		select ERROR_NUMBER(),ERROR_MESSAGE()
+	END CATCH
 END
 GO
 
@@ -44,13 +48,19 @@ CREATE PROCEDURE  UpdateGasto
 	@importe decimal (8,2) = null
 AS
 BEGIN
-	update gasto 
+	BEGIN TRY
+		update gasto 
 	set
         periodo = CASE WHEN @periodo IS NOT NULL THEN @periodo ELSE periodo END,
         fechapago = CASE WHEN @fecha_pago IS NOT NULL THEN @fecha_pago ELSE fechapago END,
         idtipogasto = CASE WHEN @id_tipogasto IS NOT NULL THEN @id_tipoGasto ELSE idtipogasto END,
         importe = CASE WHEN @importe IS NOT NULL THEN @importe ELSE importe END
     WHERE idgasto = @id_gasto;
+	END TRY
+	BEGIN CATCH
+		select ERROR_NUMBER(),ERROR_MESSAGE()
+	END CATCH
+	
 END
 GO
 
@@ -68,8 +78,14 @@ CREATE PROCEDURE  DeleteGasto
 	@id_gasto int
 AS
 BEGIN
-	 DELETE FROM gasto
-    WHERE idgasto = @id_gasto;
+	BEGIN TRY
+		 DELETE FROM gasto
+		 WHERE idgasto = @id_gasto;
+	END TRY
+	BEGIN CATCH
+		select ERROR_NUMBER(),ERROR_MESSAGE()
+	END CATCH
+	
 END
 GO
 
@@ -79,19 +95,175 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
+/*
+	Función que calcula el gasto total de un consorcio en un determinado año
+*/
 CREATE FUNCTION TotalConsorcioPorAnio
 (
 	@id_provincia int,
 	@id_localidad int,
 	@id_consorcio int,
 	@anio int
-) RETURNS decimal(8,2) 
+) RETURNS table
 AS 
+RETURN 
+(
+	   select distinct g.idprovincia as Provincia,
+	   g.idlocalidad as Localidad,
+	   g.idconsorcio as Consorcio,
+	   sum(g.importe) as Total
+	   from gasto g
+	   where g.idprovincia=@id_provincia and g.idlocalidad=@id_localidad and g.idconsorcio=@id_consorcio and year(g.fechapago)=@anio
+	   group by g.idprovincia,g.idlocalidad,g.idconsorcio
+);
+go
+
+
+/*
+	Función que calcula el gasto total por tipo de gasto de un consorcio en un determinado año
+*/
+CREATE FUNCTION TotalConsorcioPorAnioTipoGasto
+(
+	@id_provincia int,
+	@id_localidad int,
+	@id_consorcio int,
+	@anio int
+) RETURNS table
+AS 
+RETURN 
+(
+	   select distinct g.idprovincia as Provincia,
+	   g.idlocalidad as Localidad,
+	   g.idconsorcio as Consorcio,
+	   tg.descripcion as TipoGasto,
+	   sum(g.importe) as Total
+	   from gasto g
+	   join tipogasto tg on g.idtipogasto = tg.idtipogasto
+	   where g.idprovincia=@id_provincia and g.idlocalidad=@id_localidad and g.idconsorcio=@id_consorcio and year(g.fechapago)=@anio
+	   group by g.idprovincia,g.idlocalidad,g.idconsorcio, tg.descripcion
+);
+go
+
+/*
+	Función que calcula el gasto total en un determinado consorcio entre 2 fechas
+*/
+
+CREATE FUNCTION TotalConsorcioEntreFechas
+(
+	@id_provincia int,
+	@id_localidad int,
+	@id_consorcio int,
+	@desde date,
+	@hasta date
+) RETURNS table
+AS 
+RETURN 
+(
+	   select distinct g.idprovincia as Provincia,
+	   g.idlocalidad as Localidad,
+	   g.idconsorcio as Consorcio,
+	   sum(importe) as Total
+	   from gasto g
+	   where g.idprovincia=@id_provincia and g.idlocalidad=@id_localidad and g.idconsorcio=@id_consorcio and g.fechapago between @desde and @hasta
+	   group by g.idprovincia,g.idlocalidad,g.idconsorcio
+);
+
+go
+
+/*
+	Función que calcula la edad de un concerje
+*/
+
+create function FuncionCalcularEdad
+(
+	@idconserje int
+)returns int
+as
+begin
+	declare @edad int
+	select @edad=datediff(year,c.fechnac,GETDATE()) from conserje c
+	where c.idconserje=@idconserje
+	return @edad
+end
+go
+
+/*
+	Procedimiento que calcula la edad de la tabla concerje
+*/
+
+CREATE PROCEDURE sp_CalcularEdadConBucle
+(
+    @idconserje INT 
+)
+AS
 BEGIN
-	DECLARE @Total decimal (8,2);
-	SELECT @Total = SUM(importe)
-    FROM gasto
-    WHERE idprovincia=@id_provincia and idlocalidad=@id_localidad and idconsorcio=@id_consorcio and YEAR(fechapago)=@anio;
-	RETURN ISNULL(@Total, 0);
-END;
+    --Tabla temporal
+    DECLARE @Edades TABLE
+    (
+		nombre varchar(100),
+        edad INT
+    );
+
+   
+    DECLARE @id INT;
+    DECLARE @edad INT;
+	DECLARE @nombre varchar(100);
+
+    -- Cursor
+    DECLARE cur CURSOR FOR
+    SELECT c.idconserje
+    FROM conserje c
+    WHERE c.idconserje >= @idconserje 
+    ORDER BY c.idconserje; 
+
+
+    OPEN cur;
+    FETCH NEXT FROM cur INTO @id;
+
+    
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        
+        SELECT @edad = DATEDIFF(YEAR, c.fechnac, GETDATE()),@nombre=c.apeynom
+        FROM conserje c
+        WHERE c.idconserje = @id;
+
+        INSERT INTO @Edades (nombre, edad)
+        VALUES (@nombre, @edad);
+
+        FETCH NEXT FROM cur INTO @id;
+    END
+ 
+    CLOSE cur;
+    DEALLOCATE cur;
+
+    SELECT * FROM @Edades;
+END
+GO
+------------------------------------------------------------------------------------------------------------------------------------------
+
+--Prueba de insert
+-- Insert con sentencia SQL
+INSERT INTO gasto (idprovincia,idlocalidad,idconsorcio,periodo,fechapago,idtipogasto,importe) VALUES (24,14,5,8,'20170814',3,51399.37);
+-- Insert pro procedimiento
+Exec dbo.InsertGasto 24,14,5,8,'20170814',3,51399.37;
+
+select * from gasto order by idgasto desc
+-- Update pro procedimiento
+Exec dbo.UpdateGasto 8002,8,'20170814',4,10000.00;
+-- Delete pro procedimiento
+Exec dbo.DeleteGasto 8002;
+
+--Prueba de funciones
+select * from  dbo.TotalConsorcioPorAnio(1,2,2,2013);
+
+select * from  dbo.TotalConsorcioPorAnioTipoGasto(1,2,2,2013);
+
+select * from dbo.TotalConsorcioEntreFechas(1,2,2,'2013-01-01','2017-12-31')
+
+
+---Procedimientos vs funciones
+--Procedimiento 
+exec dbo.sp_CalcularEdadConBucle @idconserje=1
+--Funcion
+select c.apeynom ,dbo.FuncionCalcularEdad(c.idconserje) as edad  from conserje c
